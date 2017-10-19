2008dbfb35107e11c0ea24c5ce516e544fafa473
1412144 <truonghai.itus@gmail.com>
2017-10-20 00:13:22 +0700

    add name and avatar to user

--- MyMes/app/models/user.rb
+++ MyMes/app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_attached_file :avatar, styles: { small: "64x64", med: "100x100", large: "300x300" }
+  validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png"]
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
 end
